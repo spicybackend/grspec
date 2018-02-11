9c4068a1adbaa289930ec633a03ba52e00d6215a
yumoose <jordane.lew@gmail.com>
2018-02-11 22:01:45 +1300

    Untracked files also included in a simple diff run

--- lib/find_changed_files.rb
+++ lib/find_changed_files.rb
@@ -1,4 +1,6 @@
 class FindChangedFiles
+  attr_reader :args
+
   GIT_DIFF_COMMAND = 'git diff'.freeze
   GIT_DIFF_OPTIONS = '--name-only'.freeze
 
@@ -7,16 +9,27 @@ class FindChangedFiles
   end
 
   def call
-    differed_files.split("\n")
+    changed_files = differed_files
+    changed_files += untracked_files if simple_diff?
+
+    changed_files.split("\n")
   end
 
   private
 
+  def simple_diff?
+    args.none?
+  end
+
   def stringified_args
-    @args.join(' ')
+    args.join(' ')
   end
 
   def differed_files
     `#{GIT_DIFF_COMMAND} #{stringified_args} #{GIT_DIFF_OPTIONS}`
+  end
+
+  def untracked_files
+    `git ls-files . --exclude-standard --others`
   end
 end
