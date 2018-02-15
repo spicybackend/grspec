bd9ae75efce099d322050007ee94a4bd5635fde8
yumoose <jordane.lew@gmail.com>
2018-02-15 21:24:03 +1300

    Add merge-base handling for diffs between refs
    Resolves #4

--- lib/find_changed_files/between_refs.rb
+++ lib/find_changed_files/between_refs.rb
@@ -9,14 +9,22 @@ class FindChangedFiles::BetweenRefs < FindChangedFiles
   end
 
   def call
-    differed_files
+    differed_files(merge_base_ref, diff_ref)
   end
 
   private
 
-  def differed_files
+  def merge_base_ref
+    merge_base = `#{GIT_MERGE_BASE_COMMAND} #{base_ref} #{diff_ref} #{REDIRECT_STDERR_TO_STDOUT}`
+
+    raise ArgumentError.new("Bad git diff arguments; #{base_ref} #{diff_ref}") unless $?.success?
+
+    merge_base.strip
+  end
+
+  def differed_files(from_ref, to_ref)
     @differed_files ||= begin
-      diff_output = `#{GIT_DIFF_COMMAND} #{base_ref} #{diff_ref} #{GIT_DIFF_OPTIONS} #{REDIRECT_STDERR_TO_STDOUT}`
+      diff_output = `#{GIT_DIFF_COMMAND} #{from_ref} #{to_ref} #{GIT_DIFF_OPTIONS} #{REDIRECT_STDERR_TO_STDOUT}`
 
       raise ArgumentError.new("Bad git diff arguments; #{base_ref} #{diff_ref}") unless $?.success?
 
--- spec/lib/find_changed_files/between_refs_spec.rb
+++ spec/lib/find_changed_files/between_refs_spec.rb
@@ -16,68 +16,109 @@ RSpec.describe FindChangedFiles::BetweenRefs do
   end
 
   describe '#call' do
-    before do
-      `git init`
-    end
+    let(:base_ref) { 'HEAD~1' }
+    let(:diff_ref) { 'HEAD' }
 
     let(:initial_file) { 'initial_file' }
+    let(:file) { 'file' }
+    let(:another_file) { 'another_file' }
 
     before do
+      `git init`
+
       `touch #{initial_file}`
       `git add ./#{initial_file}`
       `git commit -m 'added #{initial_file}'`
+
+      `touch #{file}`
+      `git add ./#{file}`
+      `git commit -m 'added #{file}'`
+
+      `touch #{another_file}`
+      `git add ./#{another_file}`
+      `git commit -m 'added #{another_file}'`
     end
 
-    context 'as a range' do
-      let(:base_ref) { 'HEAD~5' }
+    context 'between the two most recent commits' do
+      it 'finds files modified in the most recent commit' do
+        expect(changed_files).to include another_file
+      end
+    end
+
+    context 'between a range of a single commit' do
+      let(:base_ref) { 'HEAD~2' }
+      let(:diff_ref) { 'HEAD~1' }
+
+      it 'finds files modified in the commit' do
+        expect(changed_files).to include file
+      end
+    end
+
+    context 'between a range over multiple comits' do
+      let(:base_ref) { 'HEAD~2' }
       let(:diff_ref) { 'HEAD' }
 
-      context 'a bad git diff range' do
-        it 'raises a GitDiffError' do
-          expect { changed_files }.to raise_exception(ArgumentError, /git diff arguments; #{base_ref} #{diff_ref}/)
+      it 'finds files modified between the commits' do
+        expect(changed_files).to include(file, another_file)
+      end
+    end
+
+    context 'between diverged branches' do
+      let(:branch_a) { 'a' }
+      let(:branch_b) { 'b' }
+
+      let(:file_in_branch_a) { 'some_file' }
+      let(:file_in_branch_b) { 'some_other_file' }
+
+      before do
+        `git checkout HEAD~2 2>&1`
+
+        `git checkout -b #{branch_a} 2>&1`
+        `touch #{file_in_branch_a}`
+        `git add #{file_in_branch_a}`
+        `git commit -m 'added #{file_in_branch_a}'`
+
+        `git checkout - 2>&1`
+
+        `git checkout -b #{branch_b} 2>&1`
+        `touch #{file_in_branch_b}`
+        `git add #{file_in_branch_b}`
+        `git commit -m 'added #{file_in_branch_b}'`
+      end
+
+      context 'when diffing from "a" to "b"' do
+        let(:base_ref) { branch_a }
+        let(:diff_ref) { branch_b }
+
+        it 'finds the files changed since the divergence, present in "b"' do
+          expect(changed_files).to include file_in_branch_b
+        end
+
+        it 'has no files from branch "a"' do
+          expect(changed_files).to_not include file_in_branch_a
         end
       end
 
-      context 'a diff range that exists' do
-        let(:file) { 'file' }
-        let(:another_file) { 'another_file' }
+      context 'when diffing from "b" to "a"' do
+        let(:base_ref) { branch_b }
+        let(:diff_ref) { branch_a }
 
-        let(:base_ref) { 'HEAD~1' }
-        let(:diff_ref) { 'HEAD' }
-
-        before do
-          `touch #{file}`
-          `git add ./#{file}`
-          `git commit -m 'added #{file}'`
-
-          `touch #{another_file}`
-          `git add ./#{another_file}`
-          `git commit -m 'added #{another_file}'`
+        it 'finds the files changed since the divergence, present in "a"' do
+          expect(changed_files).to include file_in_branch_a
         end
 
-        context 'between HEAD~1 and HEAD' do
-          it 'finds files modified in the most recent commit' do
-            expect(changed_files).to include another_file
-          end
+        it 'has no files from branch "b"' do
+          expect(changed_files).to_not include file_in_branch_b
         end
+      end
+    end
 
-        context 'between HEAD~2 and HEAD~1' do
-          let(:base_ref) { 'HEAD~2' }
-          let(:diff_ref) { 'HEAD~1' }
+    context 'a non-existent diff range' do
+      let(:base_ref) { 'HEAD~100' }
+      let(:diff_ref) { 'HEAD' }
 
-          it 'finds files modified in the commit before last' do
-            expect(changed_files).to include file
-          end
-        end
-
-        context 'between HEAD~2 and HEAD' do
-          let(:base_ref) { 'HEAD~2' }
-          let(:diff_ref) { 'HEAD' }
-
-          it 'finds files modified in the last two commits' do
-            expect(changed_files).to include(file, another_file)
-          end
-        end
+      it 'raises a GitDiffError' do
+        expect { changed_files }.to raise_exception(ArgumentError, /git diff arguments; #{base_ref} #{diff_ref}/)
       end
     end
   end
