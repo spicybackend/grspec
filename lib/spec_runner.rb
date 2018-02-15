503f9765a8b58da483c008d7310d96fa3c7e14d0
Jordane Lew <jordane.lew@fluxfederation.com>
2018-02-16 11:38:46 +1300

    Stop spec runner from trying to run removed files
    fixes #6

--- lib/spec_runner.rb
+++ lib/spec_runner.rb
@@ -2,6 +2,10 @@ class SpecRunner
   RSPEC_COMMAND = 'rspec'.freeze
 
   def self.run(matching_specs)
-    exec "#{RSPEC_COMMAND} #{matching_specs.join(' ')}" if matching_specs.any?
+    specs_to_run, removed_files = matching_specs.partition { |spec_file| File.file?(spec_file) }
+
+    system "#{RSPEC_COMMAND} #{specs_to_run.join(' ')}" if specs_to_run.any?
+
+    [specs_to_run, removed_files]
   end
 end
--- spec/lib/spec_runner_spec.rb
+++ spec/lib/spec_runner_spec.rb
@@ -1,37 +1,82 @@
 require 'spec_helper'
+require './spec/support/temp_git_directory_support'
 require './lib/spec_runner'
 
 RSpec.describe SpecRunner do
+  include TempGitDirectorySupport
+
   let(:spec_files) { Array.new }
   let(:runner) { described_class }
   let(:run) { described_class.run(spec_files) }
 
   describe '.run' do
-    context 'there are matching specs to run' do
-      let(:spec_file) { 'spec/file' }
+    context 'there are matching spec(s) to run' do
+      before do
+        spec_files.each do |spec_file|
+          `touch #{spec_file}`
+        end
+      end
+
+      let(:spec_file) { 'file_spec.rb' }
       let(:spec_files) { [ spec_file ] }
 
-      it 'executes rspec with the spec filename' do
-        expect(runner).to receive(:exec).with("rspec #{spec_file}")
+      it 'runs the specs' do
+        expect(runner).to receive(:system).with("rspec #{spec_file}")
 
         run
       end
 
-      context 'multiple specs' do
-        let(:another_spec_file) { 'spec/another/file' }
-        let(:spec_files) { [ spec_file, another_spec_file ] }
+      context 'when the spec has been removed' do
+        before do
+          `rm #{spec_file}`
+        end
 
-        it 'executes rspec with the spec filenames' do
-          expect(runner).to receive(:exec).with("rspec #{spec_file} #{another_spec_file}")
+        it 'skips running the spec' do
+          expect(runner).to_not receive(:system).with("rspec #{spec_file}")
 
           run
+        end
+      end
+
+      context 'multiple specs' do
+        let(:another_spec_file) { 'another_file_spec.rb' }
+        let(:spec_files) { [ spec_file, another_spec_file ] }
+
+        it 'runs the specs' do
+          expect(runner).to receive(:system).with("rspec #{spec_file} #{another_spec_file}")
+
+          run
+        end
+
+        context 'when one of the specs have been removed' do
+          before do
+            `rm #{spec_files.first}`
+          end
+
+          it 'still runs the remaining spec(s)' do
+            expect(runner).to receive(:system).with("rspec #{another_spec_file}")
+
+            run
+          end
+        end
+
+        context 'when all spec files have been removed' do
+          before do
+            spec_files.each do |spec_file|
+              `rm #{spec_file}`
+            end
+          end
+
+          it 'does not execute rspec' do
+            expect(runner).to_not receive(:system).with(anything)
+          end
         end
       end
     end
 
     context 'there are no matching specs to run' do
       it 'does not execute rspec' do
-        expect(runner).to_not receive(:exec).with(anything)
+        expect(runner).to_not receive(:system).with(anything)
       end
     end
   end
